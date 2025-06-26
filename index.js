186cedfa568777d96915c042eb44fcd3f39e928a
Oséias BC <piweto2024@gmail.com>
2025-06-26 09:30:00 +0100

    Teste realizado com sucesso (notifivao assincrona da PayPay

--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 const { app } = require("./src/app")
 const PORT = process.env.PORT;
 
-//const {executePayPayPayment} = require('./src/app/services/paypay')
-//executePayPayPayment(1, 'pagamento de bilhetes', `${Date.now()}`)
 app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`))
